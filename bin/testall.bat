f6a9a1de7fef3c06e64db195157d4429fb68dfde
sakesun <sakesun@gmail.com>
2023-12-17 13:19:12 +0700

    new structure (rye & multi languages)

--- bin/testall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call %~dp0theTop-setenv.bat
-
-python -m unittest discover -v -s %THETOP_HOME%src\python
--- bin/theTop-activate.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call %~dp0theTop-setenv.bat
-%THETOP_HOME%Scripts\activate.bat
